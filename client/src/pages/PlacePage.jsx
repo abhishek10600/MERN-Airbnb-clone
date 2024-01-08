b14654d485cfeff352372e4dd2ee3d97d9bfb6cd
abhishek10600 <abhi10sheksharma@gmail.com>
2024-01-08 13:47:39 +0530

    Booking functionality left.

--- client/src/pages/PlacePage.jsx
+++ client/src/pages/PlacePage.jsx
@@ -1,10 +1,12 @@
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import BookingWidget from "../components/BookingWidget";
 
 const PlacePage = () => {
   const [place, setPlace] = useState([]);
   const { id } = useParams();
+  const [showAllPhotos, setShowAllPhotos] = useState(false);
   const getPlaceDetails = async (id) => {
     const res = await axios.get(
       `http://localhost:4000/api/v1/hotels/getHotelById/${id}`
@@ -16,23 +18,84 @@ const PlacePage = () => {
   useEffect(() => {
     getPlaceDetails(id);
   }, [id]);
+  if (showAllPhotos) {
+    return (
+      <div className="absolute inset-0 bg-black text-white min-h-screen">
+        <div className="bg-black p-8 grid gap-4">
+          <div>
+            <h2 className="text-3xl">Photos of {place.title}</h2>
+            <button
+              onClick={() => setShowAllPhotos(false)}
+              className="fixed right-12 top-8 flex gap-1 py-2 px-4 rounded-2xl shadow shadow-black bg-white text-black"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18 18 6M6 6l12 12"
+                />
+              </svg>
+              Close Photos
+            </button>
+          </div>
+          {place?.photos?.length > 0 &&
+            place.photos.map((photo) => (
+              <div key={photo} className="">
+                <img
+                  className="min-w-full"
+                  src={`http://localhost:4000/uploads/` + photo}
+                />
+              </div>
+            ))}
+        </div>
+      </div>
+    );
+  }
   return (
-    <div className="mt-4 bg-gray-100 -mx-8 px-8 py-8">
+    <div className="mt-4 bg-gray-100 -mx-8 px-8 pt-8">
       <h1 className="text-3xl">{place.title}</h1>
       <a
         target="_blank"
         href={`https://maps.google.com/?q=${place.address}`}
-        className="my-2 block font-semibold underline"
+        className="flex gap-1 my-3 block font-semibold underline"
       >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+          />
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+          />
+        </svg>
+
         {place.address}
       </a>
       <div className="relative">
-        <div className="grid gap-2 grid-cols-[2fr_1fr]">
+        <div className="grid gap-2 grid-cols-[2fr_1fr] rounded-3xl overflow-hidden">
           <div>
             {place.photos?.[0] && (
               <div>
                 <img
-                  className="aspect-square object-cover"
+                  onClick={() => setShowAllPhotos(true)}
+                  className="aspect-square object-cover cursor-pointer"
                   src={`http://localhost:4000/uploads/${place.photos[0]}`}
                 />
               </div>
@@ -41,21 +104,26 @@ const PlacePage = () => {
           <div className="">
             {place.photos?.[1] && (
               <img
-                className="aspect-square object-cover"
+                onClick={() => setShowAllPhotos(true)}
+                className="aspect-square object-cover cursor-pointer"
                 src={`http://localhost:4000/uploads/${place.photos[1]}`}
               />
             )}
             <div className="overflow-hidden">
               {place.photos?.[2] && (
                 <img
-                  className="aspect-square object-cover relative top-4"
+                  onClick={() => setShowAllPhotos(true)}
+                  className="aspect-square object-cover relative top-4 cursor-pointer"
                   src={`http://localhost:4000/uploads/${place.photos[2]}`}
                 />
               )}
             </div>
           </div>
         </div>
-        <button className="flex items-center justify-center gap-1 absolute bottom-2 right-2 py-2 px-4 bg-white rounded-2xl shadow shadow-md shadow-gray-500">
+        <button
+          onClick={() => setShowAllPhotos(true)}
+          className="flex items-center justify-center gap-1 absolute bottom-2 right-2 py-2 px-4 bg-white rounded-2xl shadow shadow-md shadow-gray-500"
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -72,6 +140,28 @@ const PlacePage = () => {
           </svg>
           Show more photos
         </button>
+      </div>
+      <div className="mt-8 grid gap-8 grid-cols-1 md:grid-cols-[2fr_1fr]">
+        <div>
+          <div className="my-4">
+            <h2 className="font-semibold text-2xl">Description</h2>
+            {place.description}
+          </div>
+          Check-in: {place.checkIn}
+          <br />
+          Check-out: {place.checkOut}
+          <br />
+          Max number of people: {place.maxGuests}
+        </div>
+        <BookingWidget place={place} />
+      </div>
+      <div className="bg-white -mx-8 px-8 py-8 mt-4 border-t">
+        <div>
+          <h2 className="font-semibold text-2xl">Extra Info</h2>
+        </div>
+        <div className="mb-4 mt-2 text-sm text-gray-700 leadgin-5">
+          {place.extraInfo}
+        </div>
       </div>
     </div>
   );
